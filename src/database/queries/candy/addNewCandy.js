d61b169aaec3442ade9049eb72157d719d007cb4
Haitham abu lamdi <hayhtam14@gmail.com>
2023-04-06 12:48:53 +0200

    Fixes

--- src/database/queries/candy/addNewCandy.js
+++ src/database/queries/candy/addNewCandy.js
@@ -3,7 +3,7 @@ const connection = require('../../config/connection');
 const addCandy = (candyData)=>{
     const { name, quantity,img_url,category_id,flavor_id } = candyData;
     let sql = {
-        text:'insert into candy (name, quantity,img_url,category_id,flavor_id) values ($1,$2,$3,$4,$5) retuning id,name,quantity,img_url,category_id,flavor_id',
+        text:'insert into candy (name, quantity,img_url,category_id,flavor_id) values ($1,$2,$3,$4,$5) returning id,name,quantity,img_url,category_id,flavor_id',
         values:[name, quantity,img_url,category_id,flavor_id]
     }
     return connection.query(sql);
